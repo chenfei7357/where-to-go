15976b7dcaca75238016d670b71d0ab6e02fea9e
chenfei <969154227@qq.com>
2019-03-21 16:10:21 +0800

    去哪儿项目

---
+++ where-to-go-web/src/main/java/com/chenfei/where/to/go/WhereToGoApplication.java
@@ -0,0 +1,14 @@
+package com.chenfei.where.to.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereToGoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WhereToGoApplication.class, args);
+    }
+
+}
