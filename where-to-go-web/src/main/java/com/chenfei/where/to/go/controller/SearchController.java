693186107ff16127ad5da4f72641849b42f4eac7
lixt <740370817@qq.com>
2019-03-22 16:10:24 +0800

    add

---
+++ where-to-go-web/src/main/java/com/chenfei/where/to/go/controller/SearchController.java
@@ -0,0 +1,39 @@
+package com.chenfei.where.to.go.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author lixt
+ * @date 2019/3/21 下午6:42
+ * @comment
+ */
+
+@Controller
+@RequestMapping({"search"})
+@Api(description = "搜索附近")
+public class SearchController {
+
+    String lng = "121.456798";//经度
+    String lat = "31.230647";//纬度
+
+
+    //周边检索
+    //http://api.map.baidu.com/place/search?query=海底捞
+    // &location=31.204055632862,121.41117785465&radius=1000&region=上海&output=html&src=webapp.baidu.openAPIdemo
+    @ApiOperation(value = "搜索附近美食", notes = "搜索附近美食")
+    @RequestMapping(value = {"/eat-{name}"}, method = {RequestMethod.GET})
+    public String searchEat(@PathVariable(value = "name", required = true) String name) throws Exception {
+        String msg = name;
+        msg = new String(msg.getBytes("GBK"), "iso-8859-1");
+        return "redirect:http://api.map.baidu.com/place/search?query=" + msg +
+                "&location=" + lat + "," + lng + "&radius=1000&output=html&coord_type=bd09ll&src=webapp.baidu.openAPIdemo";
+    }
+
+
+
+}
--- where-to-go-web/src/main/java/com/chenfei/where/to/go/controller/TestController.java
+++ where-to-go-web/src/main/java/com/chenfei/where/to/go/controller/TestController.java
@@ -23,4 +23,44 @@ public class TestController {
     public String getUserDetailByAccount() {
         return "hello ！";
     }
+
+
+
+
+
+
+//    public static String getLngLat(String addrName){
+//        //http://www.nmc.cn/f/rest/province全国各省份
+//        JSONObject object = new JSONObject();
+//        //创建客户端
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        String entityr = "";
+//        //创建Get实例
+//        HttpGet httpGet = new HttpGet("http://api.map.baidu.com/geocoder?address="+addrName+"&output=json&coord_type=bd09ll&src=webapp.baidu.openAPIdemo");
+//        //添加请求头的信息，模拟浏览器访问
+//        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3573.0 Safari/537.36");
+//
+//        try{
+//            //获得Response
+//            CloseableHttpResponse response = httpClient.execute(httpGet);
+//            if(response.getStatusLine().getStatusCode() == 200){
+//                //当响应状态码为200时，获得该网页源码并打印
+//                String entity = EntityUtils.toString(response.getEntity(),"utf-8");
+//                object = (JSONObject) JSONObject.parse(entity);
+//                entityr = object.getJSONObject("result").get("location").toString();
+//            }
+//
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//        System.out.println(entityr.toString());
+//        return entityr;
+//    }
+
+
+
+//    public static void main(String[] args) throws Exception {
+//        getLngLat("上海嘉里中心");
+//    }
+
 }
