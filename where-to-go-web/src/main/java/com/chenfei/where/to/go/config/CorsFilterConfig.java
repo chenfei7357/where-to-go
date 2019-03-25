cf2f91efeb205984e387b02e1b74d7f848c25111
chenfei <969154227@qq.com>
2019-03-25 11:44:17 +0800

    修改跨域过滤器改成内部类

--- where-to-go-web/src/main/java/com/chenfei/where/to/go/config/CorsFilterConfig.java
+++ where-to-go-web/src/main/java/com/chenfei/where/to/go/config/CorsFilterConfig.java
@@ -1,9 +1,19 @@
 package com.chenfei.where.to.go.config;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +32,35 @@ public class CorsFilterConfig {
         urlPatterns.add("/*");
         registrationBean.setUrlPatterns(urlPatterns);
         return registrationBean;
+    }
+
+    public class CorsFilter implements Filter {
+
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+
+        }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletRequest req = (HttpServletRequest) request;
+            HttpServletResponse resp = (HttpServletResponse) response;
+            String origin = req.getHeader("Origin");
+            if(StringUtils.isNotEmpty(origin)){
+                resp.setHeader("Access-Control-Allow-Origin", "*");
+                resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, GET");
+                resp.setHeader("Access-Control-Max-Age", "3600");
+                resp.setHeader("Access-Control-Allow-Headers", "x-requested-with,x-token");
+            }
+            resp.addHeader("x-token",req.getHeader("x-token"));
+            chain.doFilter(req, resp);
+
+        }
+
+        @Override
+        public void destroy() {
+
+        }
     }
 }
