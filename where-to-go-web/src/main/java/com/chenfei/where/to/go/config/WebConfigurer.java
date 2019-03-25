6cc87302de62743b88584db0ba9ffa9e5b7b2825
chenfei <969154227@qq.com>
2019-03-25 14:58:57 +0800

    1、添加分页插件 2、修复加入权限验证，swagger无法访问的bug

--- where-to-go-web/src/main/java/com/chenfei/where/to/go/config/WebConfigurer.java
+++ where-to-go-web/src/main/java/com/chenfei/where/to/go/config/WebConfigurer.java
@@ -14,7 +14,8 @@ public class WebConfigurer implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(
                 new LoginInterceptor()
-        ).addPathPatterns("/**");
+        ).addPathPatterns("/**")
+        .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
     }
 
 }
