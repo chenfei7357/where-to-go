38daaf584d6f82293517e9682847b2079d2be9fd
chenfei <969154227@qq.com>
2019-04-04 18:21:04 +0800

    1、集成dubbo 2、添加client模块，来统一接收dubbo接口

--- where-to-go-service/src/main/java/com/chenfei/where/to/go/service/dubboApiServiceImpl/ConfigsDubboApiServiceImpl.java
+++ where-to-go-service/src/main/java/com/chenfei/where/to/go/service/dubboApiServiceImpl/ConfigsDubboApiServiceImpl.java
@@ -3,19 +3,24 @@ package com.chenfei.where.to.go.service.dubboApiServiceImpl;
  * Created by chenfei on 2019/4/4 17:08
  */
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.chenfei.where.to.go.dubboApiFacade.ConfigsDubboApiFacade;
 import com.chenfei.where.to.go.model.vo.ConfigsVO;
+import com.chenfei.where.to.go.response.CommonResponseUtils;
 import com.chenfei.where.to.go.response.CommonResultResponse;
+import com.chenfei.where.to.go.service.ConfigsService;
 
 @Service
 public class ConfigsDubboApiServiceImpl implements ConfigsDubboApiFacade {
 
+    @Reference
+    private ConfigsService configsService;
 
     @Override
     public CommonResultResponse<ConfigsVO> queryConfigByName(String name) {
-
-        return null;
+        ConfigsVO configsVO = configsService.queryConfigByName(name);
+        return CommonResponseUtils.success(configsVO);
     }
 }
 
